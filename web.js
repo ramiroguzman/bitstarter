6ed3e97e1a84c0ec8da147d866ed2d2049e5e746
ramiroguzman <ramiroguzman@gmail.com>
2013-07-15 00:16:26 +0000

    new changes

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!2');
+ app.get('/', function(request, response) {
+ var content= fs.readFileSync('index.html');
+ response.send(content);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+   console.log("Listening on " + port);
 });
